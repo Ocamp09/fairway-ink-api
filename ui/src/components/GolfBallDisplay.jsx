1c4a84ce81a361f96c3d02eab6aa8a0404c8df42
Ocamp09 <owen19campbell@gmail.com>
2025-02-06 13:00:17 -0500

    refactor code

--- ui/src/components/GolfBallDisplay.jsx
+++ ui/src/components/GolfBallDisplay.jsx
@@ -1,81 +1,19 @@
 import "../components/GolfBallDisplay.css";
 import { useState } from "react";
 import ImageEditor from "./Image_Drawing/ImageEditor";
-import ImageScaler from "./ImageScaler";
-import STLViewer from "./3D-View/STLViewer";
-import axios from "axios";
 import TabMenu from "./TabMenu";
-import { useCart } from "./Cart/CartContext";
-import QuantityDropdown from "./QuantityDropdown";
+import ScaleSvg from "./Scale/ScaleSvg";
+import PreviewTab from "./Preview/PreviewTab";
 
 const GolfBallDisplay = () => {
-  const [scale, setScale] = useState(1);
   const [svgUrl, setSvgUrl] = useState(null);
   const [svgData, setSvgData] = useState(null);
   const [stlUrl, setStlUrl] = useState("default.stl");
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [stlKey, setStlKey] = useState(0);
   const [showDesign, setShowDesign] = useState(true);
   const [showScale, setShowScale] = useState(false);
   const [showPreview, setShowPreview] = useState(false);
   const [paths, setPaths] = useState([]);
-  const [quantity, setQuantity] = useState(1);
-
-  const { addToCart } = useCart();
-
-  // get svg width and height, scale down to size I want to display, then factor that scale into the query sent
-  const canvasSizePx = 125 * scale;
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!svgData) {
-      setError("Please select a file to upload.");
-      return;
-    }
-
-    setIsLoading(true);
-
-    const formData = new FormData();
-    formData.append(
-      "svg",
-      new Blob([svgData], { type: "image/svg+xml" }),
-      "golfball" + stlKey + ".svg"
-    );
-    formData.append("scale", scale);
-    setStlUrl("http://localhost:5001/output/stl/default.stl");
-
-    try {
-      const response = await axios.post(
-        "http://localhost:5001/generate",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-
-      if (response.data.success) {
-        setStlUrl(response.data.stlUrl);
-        setStlKey((prevKey) => prevKey + 1);
-        setShowPreview(true);
-        setShowScale(false);
-      } else {
-        setError("Error processing file. Please try again.");
-      }
-    } catch (err) {
-      setError("An error occurred while uploading the file.");
-      console.error(err);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const handleAddToCart = (event) => {
-    event.preventDefault();
-    addToCart(stlUrl, quantity);
-  };
 
   return (
     <div className="golf-ball-display">
@@ -87,87 +25,31 @@ const GolfBallDisplay = () => {
         showPreview={showPreview}
         setShowPreview={setShowPreview}
       />
-      <div className="image-body">
-        {showDesign && (
-          <div>
-            <ImageEditor
-              setSvgUrl={setSvgUrl}
-              setSvgData={setSvgData}
-              setShowDesign={setShowDesign}
-              setShowScale={setShowScale}
-              showDesign={showDesign}
-              paths={paths}
-              setPaths={setPaths}
-            ></ImageEditor>{" "}
-          </div>
-        )}
-
-        {showScale && (
-          <div>
-            <p>Scale the image to the desired size</p>
-            <div className="ball-displays">
-              <div className="golf-template">
-                {svgUrl && (
-                  <img
-                    src={svgUrl}
-                    alt="Uploaded"
-                    className="upload-img"
-                    style={{
-                      width: `${canvasSizePx}px`, // Set width based on scale
-                    }}
-                  />
-                )}
-              </div>
-              <div>
-                <p>Life Size</p>
-                <div className="golf-real-size">
-                  {svgUrl && (
-                    <img
-                      src={svgUrl}
-                      alt="Uploaded"
-                      className="upload-img"
-                      style={{
-                        width: `${(canvasSizePx * 173) / 500}px`, // Set width based on scale
-                      }}
-                    />
-                  )}
-                </div>
-              </div>
-            </div>
-            <ImageScaler scale={scale} setScale={setScale}></ImageScaler>
-            <form onSubmit={handleSubmit}>
-              <button
-                type="submit"
-                className="submit-button"
-                disabled={isLoading}
-              >
-                {isLoading ? "Processing..." : "Upload and Generate STL"}
-              </button>
-              {error && <p className="error-message">{error}</p>}
-            </form>
-          </div>
-        )}
-      </div>
-      {showPreview && (
-        <div className="stl-viewer">
-          <p>3-d Render Preview</p>
-          {stlUrl && <STLViewer key={stlKey} stlUrl={stlUrl} />}
-          <div>
-            <QuantityDropdown
-              setQuantity={setQuantity}
-              quantity={quantity}
-              maxQuantity={15}
-            />
-            <button
-              onClick={handleAddToCart}
-              className="submit-button"
-              disabled={isLoading}
-            >
-              Add to Cart
-            </button>
-          </div>
-        </div>
+      {showDesign && (
+        <ImageEditor
+          setSvgUrl={setSvgUrl}
+          setSvgData={setSvgData}
+          setShowDesign={setShowDesign}
+          setShowScale={setShowScale}
+          showDesign={showDesign}
+          paths={paths}
+          setPaths={setPaths}
+        />
       )}
+
+      {showScale && (
+        <ScaleSvg
+          svgUrl={svgUrl}
+          svgData={svgData}
+          setSvgData={setSvgData}
+          stlKey={stlKey}
+          setStlKey={setStlKey}
+          setStlUrl={setStlUrl}
+          setShowPreview={setShowPreview}
+          setShowScale={setShowScale}
+        />
+      )}
+      {showPreview && <PreviewTab stlUrl={stlUrl} />}
     </div>
   );
 };
