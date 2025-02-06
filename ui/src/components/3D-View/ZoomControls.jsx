8113a4e4e3a6b85297814efdc039e7eb231fde6b
Ocamp09 <owen19campbell@gmail.com>
2025-02-06 13:07:30 -0500

    change zoom to icons

--- ui/src/components/3D-View/ZoomControls.jsx
+++ ui/src/components/3D-View/ZoomControls.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { BiSolidZoomIn, BiSolidZoomOut } from "react-icons/bi";
+
 import "./ZoomControls.css";
 
 const ZoomControls = ({ onZoomIn, onZoomOut }) => {
   return (
     <div className="zoom-body">
-      <button onClick={onZoomIn} className="zoom-button">
-        Zoom In
+      <button onClick={onZoomIn}>
+        <BiSolidZoomIn color="black" size={32} />
       </button>
-      <button onClick={onZoomOut}>Zoom Out</button>
+      <button onClick={onZoomOut}>
+        <BiSolidZoomOut color="black" size={32} />
+      </button>
     </div>
   );
 };
