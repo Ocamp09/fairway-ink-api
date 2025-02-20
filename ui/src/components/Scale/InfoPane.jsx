4b4f17e4e71c31677c38a7678f3af4d2e28967ad
Ocamp09 <owen19campbell@gmail.com>
2025-02-20 12:57:35 -0500

    make info pane more sizeable

--- ui/src/components/Scale/InfoPane.jsx
+++ ui/src/components/Scale/InfoPane.jsx
@@ -4,16 +4,16 @@ const InfoPane = ({ warnText, redText }) => {
   return (
     <div className="info-main" hidden={!warnText && !redText}>
       {warnText && (
-        <>
+        <div className={redText ? "info-item hr" : "info-item"}>
           <div className="warning"></div>
           <p>{warnText}</p>
-        </>
+        </div>
       )}
       {redText && (
-        <>
+        <div className="info-item">
           <div className="remove"></div>
           <p>{redText}</p>
-        </>
+        </div>
       )}
     </div>
   );
