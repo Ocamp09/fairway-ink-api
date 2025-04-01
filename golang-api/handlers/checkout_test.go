7f62ea3d873c23ecdb187eb4059670d4e4fe961c
Ocamp09 <owen19campbell@gmail.com>
2025-03-31 21:06:36 -0400

    start checkout test

---
+++ golang-api/handlers/checkout_test.go
@@ -0,0 +1,102 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest/observer"
+)
+
+type requestPayload struct {
+	Cart string `form:"cart"`
+}
+
+type testFields struct {
+	desc string
+	request requestPayload
+	wantStatus int
+	wantSuccess bool
+	wantLogs []observer.LoggedEntry
+}
+
+func TestPaymentIntent(t *testing.T) {
+	tests := []testFields {
+		{
+			desc: "No request body",
+			request: requestPayload{},
+			wantStatus: http.StatusInternalServerError,
+			wantSuccess: false,
+			wantLogs: []observer.LoggedEntry{
+				{
+					Entry: zapcore.Entry{
+						Level: zapcore.ErrorLevel,
+						Message: "Error parsing request: ",
+					},
+				},
+			},
+		},
+	}
+	core, observedLogs := observer.New(zap.DebugLevel)
+	logger := zap.New(core)
+	sugar := logger.Sugar()
+
+	gin.SetMode(gin.TestMode)
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			observedLogs.TakeAll()
+
+			// set the router
+			router := gin.Default()
+			router.POST("/create-payment-intent", func(c *gin.Context) {CreatePaymentIntent(c, sugar)})
+
+			// convert payload to json
+			jsonData, err := json.Marshal(tt.request)
+			if err != nil {
+				t.Fatalf("Failed to get response json: %v", err)
+			}
+
+			// make request
+			req, err := http.NewRequest("POST", "/create-payment-intent", bytes.NewBuffer(jsonData))
+			if err != nil {
+				t.Fatalf("Failed to create http request: %v", err)
+			}
+			req.Header.Set("Content-Type", "application/json")
+
+			w := httptest.NewRecorder()
+			router.ServeHTTP(w, req)
+
+			var response map[string]interface{}
+			json.Unmarshal(w.Body.Bytes(), &response)
+
+			// assert proper status code and success message
+			assert.Equal(t, tt.wantStatus, w.Code)
+
+			if success, exists := response["success"]; exists {
+				assert.Equal(t, tt.wantSuccess, success, "Success codes do not match")
+			} else {
+				assert.False(t, tt.wantSuccess, "Expected success but key not found in response")
+			}
+
+			// assert proper logs
+			allLogs := observedLogs.All()
+			assert.Equal(t, len(tt.wantLogs), len(allLogs), "Log counts do not match")
+
+			for i, log := range tt.wantLogs {
+				if i >= len(allLogs) {
+					break
+				}
+
+				assert.Equal(t, log.Entry.Level, allLogs[i].Entry.Level, "Log levels do not match")
+				assert.Equal(t, log.Entry.Message, allLogs[i].Entry.Message, "Log messages do not match")
+			}
+		})
+	}
+}
